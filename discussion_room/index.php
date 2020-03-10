d8a9282148041f2b0359deca735315abcc2bf937
khalidi hassan thewinner <thewinner016@gmail.com>
2020-03-09 23:51:51 -0700

    updated structure

--- discussion_room/index.php
+++ discussion_room/index.php
@@ -2,17 +2,66 @@
 <html>
 <head>
 <title>discussion</title>
+<link rel="stylesheet" type="text/css" href="styles/index.css"/>
+<link rel="stylesheet" type="text/css" href="styles/topbar.css"/>
 </head>
 <body>
-<div id="topbar"></div>
+<div id="topbar">
+<img src="../media_store/onecampus_store/onecampus_icons/menu.png"/>
+</div>
 
-<!..the whiteboard starts ..>
-<?php
-include("smartboard/smartboard.php");
-?>
+<!--the whiteboard starts -->
+<div id="board">
+    <center>
+    <canvas id="canvasarea"></canvas>
+  
+
+    <div id="toolbar">
+        <img src="../media_store/onecampus_store/onecampus_icons/markerpen.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/colorchooser.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/size.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/text.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/eraser.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/rect.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/penta.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/oval.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/blacky.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/withy.png"/>
+    </div>
+    </center>
 </div>
+
+<!--end of board -->
+<!--discussion room -->
+
+<div id="discussion_room">
+    <div id="video_room">  
+    <div id="options">
+        <img src="../media_store/onecampus_store/onecampus_icons/audio1.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/chat_room.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/video1.png"/>
+        <img src="../media_store/onecampus_store/onecampus_icons/workspace.png"/>
+
+    </div>
+    <div id="participants">
+     <video src="samplevid.mp4"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+     <video src="#"></video>
+    </div>
+    </div>
+    <div id="textchat_area">
+    </div>
+    <div id="material_box">
+    </div>
 </div>
-<!..end of board ..>
 
 </body>
 </html>
